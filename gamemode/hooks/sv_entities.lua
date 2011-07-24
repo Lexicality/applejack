83b797f36958a8471079bc9d461e64a3aa54ce7a
Lexi R <lexi@lexi.org.uk>
2011-07-24 18:23:35 +0100

    s->S to fix EntityOwnerSet hook not showing up

--- gamemode/hooks/sv_entities.lua
+++ gamemode/hooks/sv_entities.lua
@@ -4,11 +4,17 @@
 --]]
 
 ---
+-- @name GM
+-- @class table
+-- @description The gamemode table.
+local GM = GM;
+
+---
 -- Called when the owner of an en entity is set.
 -- Currently very few things actaully call this even though more should.
 -- The doors plugin uses this to memorise owners for doors on the map.
 -- @param ent The entity whose owner has just been set.
 -- @param owner The new owner. This is either a nil (no owner), player, or a Team/Group/Gang struct.
-function GM:EntityOwnerset(ent, owner)
+function GM:EntityOwnerSet(ent, owner)
 	--
 end
