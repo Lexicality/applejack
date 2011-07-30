cf5fe4e7ae11087b1c69ee84e709d5157cd0c9b4
Lexi R <lexi@lexi.org.uk>
2011-07-30 19:23:12 +0100

    Made all packaging items have a batch of 5. This fixes half of them not showing up

--- gamemode/items/packaging/large_crate.lua
+++ gamemode/items/packaging/large_crate.lua
@@ -8,7 +8,6 @@ ITEM.Size			= 2;
 ITEM.Cost			= 500;
 ITEM.Model			= "models/props_junk/wood_crate002a.mdl";
 ITEM.Store			= true;
-ITEM.Batch			= 10;
 ITEM.Capacity		= 40;
 ITEM.Plural			= "Large Crates";
 ITEM.Description	= "A Large crate to put things in";
--- gamemode/items/packaging/large_crate_dark.lua
+++ gamemode/items/packaging/large_crate_dark.lua
@@ -8,7 +8,6 @@ ITEM.Size			= 2;
 ITEM.Cost			= 500;
 ITEM.Model			= "models/props_junk/wood_crate002a.mdl";
 ITEM.Store			= true;
-ITEM.Batch			= 10;
 ITEM.Capacity		= 40;
 ITEM.Skin			= 1;
 ITEM.Plural			= "Large Dark Crates";
