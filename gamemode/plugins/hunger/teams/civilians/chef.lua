20bc97ba4342ab459584f243c92b177600b7700d
Lexi R <lexi@lexi.org.uk>
2011-10-12 09:05:25 +0100

    Fiddled the teams to use the new team systema.

--- gamemode/plugins/hunger/teams/civilians/chef.lua
+++ gamemode/plugins/hunger/teams/civilians/chef.lua
@@ -1,9 +1,9 @@
-if (not GM:GetPlugin("hunger")) then
-	TEAM.Valid = false; -- Invalidate the team if there aint no plugin
-end
 TEAM.Name = "Chef";
 TEAM.Description = "A creates and sells food.";
 TEAM.Color = Color(255,125,200);
 TEAM.GroupLevel = GROUP_MERCHANT;
-TEAM.CanMake = {};
+TEAM.CanMake = {
+    CATEGORY_FOOD;
+    CATEGORY_ALCOHOL;
+};
 TEAM.SizeLimit = 4;
