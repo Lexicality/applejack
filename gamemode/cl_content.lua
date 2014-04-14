e747ee037cc2e9c3df5925785fb0f3e883a8562b
Lex Robinson <lexi@lexi.org.uk>
2014-04-14 23:08:26 +0100

    Use the GM13 IMaterial interface
    #41

--- gamemode/cl_content.lua
+++ gamemode/cl_content.lua
@@ -5,25 +5,25 @@
 --[[
 local chand		= Material	"models/weapons/v_hand/v_hand_sheet"
 local nhand		= Material	"hand_replacement"
-chand:SetMaterialTexture(	"$basetexture",	nhand:GetMaterialTexture"$basetexture"	)
-chand:SetMaterialTexture(	"$bumpmap",		nhand:GetMaterialTexture"$bumpmap"		)	
+chand:SetTexture(	"$basetexture",	nhand:GetTexture"$basetexture"	)
+chand:SetTexture(	"$bumpmap",		nhand:GetTexture"$bumpmap"		)	
 --]]
 --[[ Jayhawk's Epic Hands ]]--
 local mat = Material("models/weapons/v_hand/v_hand_sheet");
 local mot = Material("models/weapons/v_models/hands/v_hands");
 local met = Material("hand_replacement_jayhawk_v2");
 local mut = Material("css_hand_replacement_jayhawk");
-mat:SetMaterialTexture("$basetexture", met:GetMaterialTexture("$basetexture"));
-mot:SetMaterialTexture("$basetexture", mut:GetMaterialTexture("$basetexture"));
+mat:SetTexture("$basetexture", met:GetTexture("$basetexture"));
+mot:SetTexture("$basetexture", mut:GetTexture("$basetexture"));
 if (game.GetMap():lower() ~= "rp_evocity_v2d") then return end
 local cursign = Material	"maps/rp_evocity_v2pdless/sgtsicktextures/bankofamericasign_-6363_-7696_137"
 local newsign = Material	"evocityextrude"
-cursign:SetMaterialTexture(	"$basetexture",	newsign:GetMaterialTexture"$basetexture")
-cursign:SetMaterialTexture(	"$bumpmap",		newsign:GetMaterialTexture"$bumpmap"	)
+cursign:SetTexture(	"$basetexture",	newsign:GetTexture"$basetexture")
+cursign:SetTexture(	"$bumpmap",		newsign:GetTexture"$bumpmap"	)
 local cursign2	= Material	"SGTSICKTEXTURES/BANKOAMERICA2"
 newsign			= Material	"evocityintrude"
-cursign2:SetMaterialTexture("$basetexture", newsign:GetMaterialTexture"$basetexture")
-cursign2:SetMaterialTexture("$bumpmap",     newsign:GetMaterialTexture"$bumpmap"    )
-cursign2:SetMaterialInt(	"$normalmapalphaenvmapmask", 1					        )
-cursign2:SetMaterialTexture("$envmap",      cursign:GetMaterialTexture"$envmap"     )
-cursign2:SetMaterialString(	"$envmaptint",	"[ .3 .3 .45 ]")
+cursign2:SetTexture("$basetexture", newsign:GetTexture"$basetexture")
+cursign2:SetTexture("$bumpmap",     newsign:GetTexture"$bumpmap"    )
+cursign2:SetInt(	"$normalmapalphaenvmapmask", 1			        )
+cursign2:SetTexture("$envmap",      cursign:GetTexture"$envmap"     )
+cursign2:SetString(	"$envmaptint",	"[ .3 .3 .45 ]")
