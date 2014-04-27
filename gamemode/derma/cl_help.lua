cd424fcf3960d7e680732555ebaf81d4f4bd427b
Lex Robinson <lexi@lexi.org.uk>
2014-04-27 16:06:10 +0100

    Sort the help menu in a moderately sensible way. (#71)

--- gamemode/derma/cl_help.lua
+++ gamemode/derma/cl_help.lua
@@ -10,6 +10,49 @@ function PANEL:Init()
 	self:Reload();
 end
 
+local function isModerator(text)
+	return text:sub(1,  9) == "Moderator";
+end
+
+local function isAuthed(text)
+	return text:sub(1, 10) == "Superadmin"
+		or text:sub(1,  5) == "Admin"
+		or isModerator(text);
+end
+
+local function isAbuse(text)
+	return text:find("Abuse") and true or false;
+end
+
+local function bullshitSort(a, b)
+	a,b = a.title, b.title;
+	-- General always floats to the top
+	if (a == "General") then
+		return true;
+	elseif (b == "General") then
+		return false;
+	end
+	local aa, ab = isAuthed(a), isAuthed(b);
+	-- Authed commands go below normal ones.
+	if (aa ~= ab) then
+		-- If ab is true, aa is false and thus a is lighter than b. Inverse also applies
+		return ab;
+	elseif (not aa) then
+		-- aa and bb are both false. Normal sorting applies
+		return a < b;
+	end
+	aa, ab = isAbuse(a), isAbuse(b);
+	if (aa ~= ab) then
+		return ab;
+	end
+	aa, ab = isModerator(a), isModerator(b);
+	if (aa ~= ab) then
+		-- Moderator commands go upwards
+		return aa;
+	end
+	return a < b;
+end
+
 -- Reload the help text.
 function PANEL:Reload()
 	self:Empty();
@@ -25,6 +68,8 @@ function PANEL:Reload()
 			text  = text2;
 		} );
 	end
+
+	table.sort(sections, bullshitSort);
 
 	self:CreateChildren(sections);
 	self:InvalidateLayout(true);
