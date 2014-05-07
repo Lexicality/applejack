d94ab4cbaedd7074d313169e052c5149b7afe53d
Lex Robinson <lexi@lexi.org.uk>
2014-05-08 00:07:48 +0100

    Add missing _ in POLICE_WEAPONS in secretary/quartermaster. (#80)

--- gamemode/plugins/officials/teams/officials/quartermaster.lua
+++ gamemode/plugins/officials/teams/officials/quartermaster.lua
@@ -1,8 +1,8 @@
 TEAM.Name             = "Quartermaster";
 TEAM.Description      = "Supplies the police with their needs";
 TEAM.Color            = Color(50,200,255);
-TEAM.CantUse          = {CATEGORY_WEAPONS, CATEGORY_POLICEWEAPONS };
-TEAM.CanMake          = {CATEGORY_WEAPONS, CATEGORY_POLICEWEAPONS, CATEGORY_AMMO};
+TEAM.CantUse          = {CATEGORY_WEAPONS, CATEGORY_POLICE_WEAPONS };
+TEAM.CanMake          = {CATEGORY_WEAPONS, CATEGORY_POLICE_WEAPONS, CATEGORY_AMMO};
 TEAM.Models.Male      = {"models/player/Hostage/Hostage_02.mdl","models/player/Hostage/Hostage_03.mdl"};
 TEAM.Models.Female    = TEAM.Models.Male;
 TEAM.GroupLevel       = GROUP_MERCHANT;
