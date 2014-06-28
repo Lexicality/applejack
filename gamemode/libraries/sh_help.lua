73115f975f4056c507572f835a165e2e3cc51c5a
Lex Robinson <lexi@lexi.org.uk>
2014-06-28 14:23:41 +0100

    Replace DataStream in Help library with net.Send
    Fixes #86
    Part of #20

--- gamemode/libraries/sh_help.lua
+++ gamemode/libraries/sh_help.lua
@@ -14,7 +14,15 @@ function cider.help.add(cat, help)
 	end
 end
 if (CLIENT) then
-	datastream.Hook("helpReplace",function(_,_,_,data)
+	net.Receive("Moonshine Help", function()
+		local data = {};
+		for i = 1, net.ReadUInt(8) do
+			local lines = {};
+			for i = 1, net.ReadUInt(10) do
+				lines[i] = net.ReadString();
+			end
+			data[net.ReadString()] = lines;
+		end
 		cider.help.stored = data;
 		if (cider.help.panel) then
 			cider.help.panel:Reload();
@@ -27,7 +35,19 @@ else
 	cider.help.add("General", "Put .// before your message to talk in local OOC.");
 	cider.help.add("General", "Press F1 to open the main menu.");
 	cider.help.add("General", "Press F2 to open the access menu when looking at something you have access to.");
+
+	util.AddNetworkString("Moonshine Help");
 	hook.Add("PlayerInitialized", "Applejack Help Spammer", function(ply)
-		datastream.StreamToClients(ply,"helpReplace",cider.help.stored);
+		local help = cider.help.stored;
+		net.Start("Moonshine Help");
+		net.WriteUInt(table.Count(help), 8)
+		for key, lines in pairs(help) do
+			net.WriteUInt(#lines, 10);
+			for _, line in ipairs(lines) do
+				net.WriteString(line);
+			end
+			net.WriteString(key);
+		end
+		net.Send(ply);
 	end);
 end
