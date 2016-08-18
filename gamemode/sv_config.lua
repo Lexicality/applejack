b5214a487cfe86044ac0d929d4c020fc6a220e6e
Ross <PitifulCanine3@users.noreply.github.com>
2016-08-18 11:43:10 +0100

    Readded the Serverside config file.
    For some odd reason there doesn't seem to be a Serverside config file for entering the MySQL details.

---
+++ gamemode/sv_config.lua
@@ -0,0 +1,11 @@
+--[[
+	~ Serverside Config ~
+	~ Applejack ~
+--]]
+
+--Remove the commented out lines
+GM.Config["MySQL Host"] = ""; -- The host that your MySQL database is located.
+GM.Config["MySQL Username"] = ""; -- The username that you log into MySQL with.
+GM.Config["MySQL Password"] = ""; -- The password that you log into MySQL with.
+GM.Config["MySQL Database"] = ""; -- The name of the database that we'll be using.
+GM.Config["MySQL Table"] = "players"; -- The name of the table where the player data is stored.
