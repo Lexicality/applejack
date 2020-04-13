f32bd5d0cb0b34136bf44a6d6525fa1e13a23bcf
Lex Robinson <lex@lex.me.uk>
2020-04-13 22:17:01 +0100

    Cull the hunger plugin
    - Get rid of all the GTA:SA items
    - Get rid of a bunch of alcohol items I didn't like
    - Move the non-alcoholic drinks into the plugin
    
    The GTA:SA items are cool but there's a lot of them and fuck all the
    extra content download

--- gamemode/plugins/hunger/items/alcohol/beer.lua
+++ gamemode/plugins/hunger/items/alcohol/beer.lua
@@ -1,12 +1,12 @@
 --
--- ~ Beer~
+-- ~ Beer ~
 -- ~ Applejack ~
 --
-ITEM.Name = "Beer";
+ITEM.Name = "Bottle of Beer";
 ITEM.Cost = 10;
-ITEM.Model = "models/props_junk/glassbottle01a.mdl";
+ITEM.Model = "models/props_junk/garbage_glassbottle002a.mdl";
 ITEM.Store = true;
-ITEM.Plural = "Beers";
-ITEM.Description = "A cold Beer that restores 5 hunger";
-ITEM.Hunger = 5;
+ITEM.Plural = "Bottles of beer";
+ITEM.Description = "A bottle of beer which removes 15 hunger.";
+ITEM.Hunger = 15;
 ITEM.Base = "food";
